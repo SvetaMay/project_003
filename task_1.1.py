5064c7af27370d34cd1ef11f1c0f9313cabb67a2
Nikita Pyshkov <85805562+pyshkovni@users.noreply.github.com>
2023-04-10 11:07:44 +0300

    Выполнено

--- task_1.1.py
+++ task_1.1.py
@@ -23,3 +23,31 @@ print(my_favorite_songs[35:49])
 print(my_favorite_songs[49:67])
 print(my_favorite_songs[67:77])
     
+# Да, можно так, но давайте "попросим" Python найти индексы за нас
+# Решение с помощью индексации строк
+
+first_song = my_favorite_songs [
+    : my_favorite_songs.find(',')
+]
+
+last_song = my_favorite_songs [
+    my_favorite_songs.rfind('N') :
+]
+
+second_song = my_favorite_songs[
+     my_favorite_songs.find('S') : 
+     my_favorite_songs.find(', A')
+]
+
+previous_song = my_favorite_songs [
+    my_favorite_songs.rfind('St') : 
+    my_favorite_songs.rfind(', N')
+    ]
+
+print(first_song, last_song, second_song, previous_song)
+
+
+# Решение с помощью метода split() и индексации списков
+songs = my_favorite_songs.split(', ')
+
+print(songs[0], songs[-1], songs[1], songs[-2])
